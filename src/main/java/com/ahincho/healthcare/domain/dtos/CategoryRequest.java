e0d2b8e981b68a7f3dbd475b7c2679a43639378a
ahincho <ahincho@unsa.edu.pe>
2023-12-23 18:53:03 -0500

    Name field is required on Category

--- src/main/java/com/ahincho/healthcare/domain/dtos/CategoryRequest.java
+++ src/main/java/com/ahincho/healthcare/domain/dtos/CategoryRequest.java
@@ -1,9 +1,11 @@
 package com.ahincho.healthcare.domain.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
 @Data @Builder
 public class CategoryRequest {
+    @NotBlank(message = "Name must not blank")
     private String name;
 }
