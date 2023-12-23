2eae79a494a2b0283257a90c5ef510db794af321
ahincho <ahincho@unsa.edu.pe>
2023-12-23 14:44:04 -0500

    Renaming Entities

--- src/main/java/com/ahincho/healthcare/domain/repositories/DrugCategoryRepository.java
+++ src/main/java/com/ahincho/healthcare/domain/repositories/DrugCategoryRepository.java
@@ -1,10 +1,10 @@
 package com.ahincho.healthcare.domain.repositories;
 
-import com.ahincho.healthcare.domain.entities.DrugCategory;
+import com.ahincho.healthcare.domain.entities.DrugCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DrugCategoryRepository extends JpaRepository<DrugCategory, Integer> {
+public interface DrugCategoryRepository extends JpaRepository<DrugCategoryEntity, Integer> {
     
 }
