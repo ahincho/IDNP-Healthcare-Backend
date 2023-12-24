361724c17b1e9c54b56ab1cf042510b56a5d52b3
ahincho <ahincho@unsa.edu.pe>
2023-12-23 19:19:47 -0500

    Removing string parameter

--- src/main/java/com/ahincho/healthcare/domain/exceptions/DrugNotFoundException.java
+++ src/main/java/com/ahincho/healthcare/domain/exceptions/DrugNotFoundException.java
@@ -3,7 +3,7 @@ package com.ahincho.healthcare.domain.exceptions;
 import static com.ahincho.healthcare.domain.exceptions.DrugExceptionMessages.NOT_FOUND;
 
 public class DrugNotFoundException extends RuntimeException {
-    public DrugNotFoundException(String message) {
+    public DrugNotFoundException() {
         super(NOT_FOUND);
     }
 }
