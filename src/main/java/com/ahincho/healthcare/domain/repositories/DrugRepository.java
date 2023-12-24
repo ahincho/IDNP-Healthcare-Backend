cbbe057dae6477ace56338d6b77cb00a6bb37e28
ahincho <ahincho@unsa.edu.pe>
2023-12-23 19:04:42 -0500

    Change name of Drug Entity

--- src/main/java/com/ahincho/healthcare/domain/repositories/DrugRepository.java
+++ src/main/java/com/ahincho/healthcare/domain/repositories/DrugRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 @Repository
 public interface DrugRepository extends JpaRepository<DrugEntity, Integer> {
-    List<DrugEntity> getDrugsByDrugCategoryId(Integer integer);
+    List<DrugEntity> getDrugEntityByDrugCategoryId(Integer integer);
 }
