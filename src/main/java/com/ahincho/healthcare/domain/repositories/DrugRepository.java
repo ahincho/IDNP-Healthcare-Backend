22b786831ec70843941cb5cd14f254ef9cd4da52
ahincho <ahincho@unsa.edu.pe>
2023-12-23 19:08:57 -0500

    Recover Drug by Name

--- src/main/java/com/ahincho/healthcare/domain/repositories/DrugRepository.java
+++ src/main/java/com/ahincho/healthcare/domain/repositories/DrugRepository.java
@@ -4,8 +4,10 @@ import com.ahincho.healthcare.domain.entities.DrugEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DrugRepository extends JpaRepository<DrugEntity, Integer> {
+    Optional<DrugEntity> getDrugEntityByName(String name);
     List<DrugEntity> getDrugEntityByCategoryId(Integer integer);
 }
