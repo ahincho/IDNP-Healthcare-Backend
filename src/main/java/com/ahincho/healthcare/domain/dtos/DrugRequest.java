ff56df29a7867953f8473804fdea27bf1d23c6a1
ahincho <ahincho@unsa.edu.pe>
2023-12-23 22:27:20 -0500

    Adding Constructors with Lombok

--- src/main/java/com/ahincho/healthcare/domain/dtos/DrugRequest.java
+++ src/main/java/com/ahincho/healthcare/domain/dtos/DrugRequest.java
@@ -3,10 +3,14 @@ package com.ahincho.healthcare.domain.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DrugRequest {
     @NotBlank(message = "Name must not be blank")
     private String name;
--- src/main/java/com/ahincho/healthcare/domain/dtos/DrugResponse.java
+++ src/main/java/com/ahincho/healthcare/domain/dtos/DrugResponse.java
@@ -1,9 +1,13 @@
 package com.ahincho.healthcare.domain.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DrugResponse {
     private Integer id;
     private String name;
