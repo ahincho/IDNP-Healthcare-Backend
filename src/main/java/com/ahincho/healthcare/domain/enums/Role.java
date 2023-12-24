d5874deb5e8db9a8f1505c805798151e6c55b259
ahincho <ahincho@unsa.edu.pe>
2023-12-24 12:57:59 -0500

    Adding Name Field and Constructors

--- src/main/java/com/ahincho/healthcare/domain/enums/Role.java
+++ src/main/java/com/ahincho/healthcare/domain/enums/Role.java
@@ -1,6 +1,17 @@
 package com.ahincho.healthcare.domain.enums;
 
 public enum Role {
-    ADMIN,
-    VIEWER
+    ADMIN("ADMIN"),
+    VIEWER("VIEWER");
+    private final String name;
+    Role(String name) {
+        this.name = name;
+    }
+    public String getRoleName() {
+        return this.name;
+    }
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
