cc7b44603ffe51a6de7eeb2d5418834c89bc82d1
ahincho <ahincho@unsa.edu.pe>
2023-12-23 21:52:45 -0500

    Refactoring Drug Rest Controller

--- src/main/java/com/ahincho/healthcare/infrastructure/controllers/DrugController.java
+++ src/main/java/com/ahincho/healthcare/infrastructure/controllers/DrugController.java
@@ -1,45 +1,55 @@
-package com.ahincho.healthcare.infrastructure;
+package com.ahincho.healthcare.infrastructure.controllers;
 
 import com.ahincho.healthcare.application.services.DrugService;
+import com.ahincho.healthcare.domain.dtos.DrugRequest;
+import com.ahincho.healthcare.domain.dtos.DrugResponse;
 import com.ahincho.healthcare.domain.entities.DrugEntity;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ahincho.healthcare.domain.mappers.DrugMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.util.UriComponentsBuilder;
+import jakarta.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/drugs")
 public class DrugController {
-    @Autowired
-    private DrugService drugService;
+    private final DrugService drugService;
+    public DrugController(DrugService drugService) {
+        this.drugService = drugService;
+    }
     @GetMapping
-    public List<DrugEntity> getAll() {
-        return drugService.getAllDrugs();
+    public ResponseEntity<List<DrugResponse>> getAll() {
+        List<DrugEntity> drugEntities = drugService.getAllDrugs();
+        if (drugEntities.isEmpty()) { return ResponseEntity.noContent().build(); }
+        return ResponseEntity.ok(drugEntities.stream().map(DrugMapper::entityToResponse).toList());
     }
     @GetMapping("/category/{category}")
-    public List<DrugEntity> getByCategory(@PathVariable("category") Integer category) {
-        return drugService.getDrugsByCategoryId(category);
-    }
-    @PostMapping
-    public DrugEntity save(@RequestBody DrugEntity drugEntity) {
-        return drugService.createDrug(drugEntity);
+    public ResponseEntity<List<DrugResponse>> getByCategory(@PathVariable("category") Integer category) {
+        List<DrugEntity> drugEntities = drugService.getDrugsByCategoryId(category);
+        if (drugEntities.isEmpty()) { return ResponseEntity.noContent().build(); }
+        return ResponseEntity.ok(drugEntities.stream().map(DrugMapper::entityToResponse).toList());
     }
     @GetMapping("/{id}")
-    public DrugEntity findById(@PathVariable("id") Integer id) {
-        return drugService.findDrugById(id).orElse(null);
+    public ResponseEntity<DrugResponse> findById(@PathVariable("id") Integer id) {
+        DrugEntity drugEntity = drugService.findDrugById(id);
+        return ResponseEntity.ok(DrugMapper.entityToResponse(drugEntity));
+    }
+    @PostMapping
+    public ResponseEntity<DrugResponse> save(@RequestBody @Valid DrugRequest drugRequest, UriComponentsBuilder uriComponentsBuilder) {
+        DrugEntity drugEntity = drugService.createDrug(DrugMapper.requestToEntity(drugRequest));
+        URI uri = uriComponentsBuilder.path("/api/drugs/{id}").buildAndExpand(drugEntity.getId()).toUri();
+        return ResponseEntity.created(uri).body(DrugMapper.entityToResponse(drugEntity));
     }
     @PutMapping("/{id}")
-    public DrugEntity update(@PathVariable("id") Integer id, @RequestBody DrugEntity drugEntity) {
-        return drugService.findDrugById(id)
-                .map(savedDrug -> {
-                    savedDrug.setName(drugEntity.getName());
-                    savedDrug.setDrugCategory(drugEntity.getDrugCategory());
-                    savedDrug.setDescription(drugEntity.getDescription());
-                    return drugService.updateDrug(savedDrug);
-                }).orElse(null);
+    public ResponseEntity<Void> update(@PathVariable("id") Integer id, @RequestBody @Valid DrugRequest drugRequest) {
+        drugService.updateDrug(id, DrugMapper.requestToEntity(drugRequest));
+        return ResponseEntity.notFound().build();
     }
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Integer id) {
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
         drugService.deleteDrug(id);
+        return ResponseEntity.notFound().build();
     }
 }
