3efd1e3d3cab851c2169a9266f3229b8ba49b34d
ahincho <ahincho@unsa.edu.pe>
2023-12-23 22:33:06 -0500

    Changing Not Blank to Not Null because its Integer

--- src/main/java/com/ahincho/healthcare/domain/dtos/DrugRequest.java
+++ src/main/java/com/ahincho/healthcare/domain/dtos/DrugRequest.java
@@ -1,6 +1,7 @@
 package com.ahincho.healthcare.domain.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -14,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class DrugRequest {
     @NotBlank(message = "Name must not be blank")
     private String name;
-    @NotBlank(message = "Category identifier must not be blank")
+    @NotNull(message = "Category identifier must not be blank")
     @Positive(message = "Category identifier must be greater than 0")
     private Integer categoryId;
     @NotBlank(message = "Description must not be blank")
