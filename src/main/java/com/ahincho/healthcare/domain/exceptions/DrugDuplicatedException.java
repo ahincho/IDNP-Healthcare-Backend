6fdf51364b1d9e963a47dfbb312827234c801880
ahincho <ahincho@unsa.edu.pe>
2023-12-23 22:41:43 -0500

    Changing Runtime Exception to Exception

--- src/main/java/com/ahincho/healthcare/domain/exceptions/DrugDuplicatedException.java
+++ src/main/java/com/ahincho/healthcare/domain/exceptions/DrugDuplicatedException.java
@@ -2,7 +2,7 @@ package com.ahincho.healthcare.domain.exceptions;
 
 import static com.ahincho.healthcare.domain.exceptions.DrugExceptionMessages.DUPLICATED;
 
-public class DrugDuplicatedException extends RuntimeException {
+public class DrugDuplicatedException extends Exception {
     public DrugDuplicatedException() {
         super(DUPLICATED);
     }
--- src/main/java/com/ahincho/healthcare/domain/exceptions/DrugNotFoundException.java
+++ src/main/java/com/ahincho/healthcare/domain/exceptions/DrugNotFoundException.java
@@ -2,7 +2,7 @@ package com.ahincho.healthcare.domain.exceptions;
 
 import static com.ahincho.healthcare.domain.exceptions.DrugExceptionMessages.NOT_FOUND;
 
-public class DrugNotFoundException extends RuntimeException {
+public class DrugNotFoundException extends Exception {
     public DrugNotFoundException() {
         super(NOT_FOUND);
     }
