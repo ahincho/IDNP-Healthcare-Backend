4616d4d6cb7309b88682aef21898ceb514b53f10
ahincho <ahincho@unsa.edu.pe>
2023-12-25 17:59:37 -0500

    Adding Transactional Annotation on Writing Operations

--- src/main/java/com/ahincho/healthcare/application/services/UserService.java
+++ src/main/java/com/ahincho/healthcare/application/services/UserService.java
@@ -6,6 +6,7 @@ import com.ahincho.healthcare.domain.exceptions.UserDuplicatedUsernameException;
 import com.ahincho.healthcare.domain.exceptions.UserNotFoundException;
 import com.ahincho.healthcare.domain.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +24,7 @@ public class UserService {
         if (optionalUser.isEmpty()) { throw new UserNotFoundException(); }
         return optionalUser.get();
     }
+    @Transactional
     public UserEntity createUser(UserEntity userEntity) throws UserDuplicatedEmailException, UserDuplicatedUsernameException {
         Optional<UserEntity> optionalUserEmail = userRepository.findByEmail(userEntity.getEmail());
         if (optionalUserEmail.isPresent()) { throw new UserDuplicatedEmailException(); }
@@ -30,12 +32,14 @@ public class UserService {
         if (optionalUserUsername.isPresent()) { throw new UserDuplicatedUsernameException(); }
         return userRepository.save(userEntity);
     }
+    @Transactional
     public void updateUser(Integer id, UserEntity userEntity) throws UserNotFoundException {
         Optional<UserEntity> optionalUser = userRepository.findById(id);
         if (optionalUser.isEmpty()) { throw new UserNotFoundException(); }
         userEntity.setId(id);
         userRepository.save(userEntity);
     }
+    @Transactional
     public void deleteUser(Integer id) throws UserNotFoundException {
         Optional<UserEntity> optionalUser = userRepository.findById(id);
         if (optionalUser.isEmpty()) { throw new UserNotFoundException(); }
