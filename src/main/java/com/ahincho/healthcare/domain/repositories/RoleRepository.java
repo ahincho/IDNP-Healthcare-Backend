9b941cf43f611b2bc6ef39af4e2ed7415f7f08af
ahincho <ahincho@unsa.edu.pe>
2023-12-25 17:30:50 -0500

    Unused Import

--- src/main/java/com/ahincho/healthcare/domain/repositories/RoleRepository.java
+++ src/main/java/com/ahincho/healthcare/domain/repositories/RoleRepository.java
@@ -2,7 +2,6 @@ package com.ahincho.healthcare.domain.repositories;
 
 import com.ahincho.healthcare.domain.entities.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
