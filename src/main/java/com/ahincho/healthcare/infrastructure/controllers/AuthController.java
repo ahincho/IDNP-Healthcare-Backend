e006adf085b9e72aa896aee5b79f99cc799120dc
ahincho <ahincho@unsa.edu.pe>
2023-12-25 18:37:52 -0500

    Auth Rest Controller

---
+++ src/main/java/com/ahincho/healthcare/infrastructure/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package com.ahincho.healthcare.infrastructure.controllers;
+
+import com.ahincho.healthcare.application.services.UserService;
+import com.ahincho.healthcare.domain.dtos.UserRequest;
+import com.ahincho.healthcare.domain.dtos.UserResponse;
+import com.ahincho.healthcare.domain.entities.UserEntity;
+import com.ahincho.healthcare.domain.exceptions.RoleNotFoundException;
+import com.ahincho.healthcare.domain.exceptions.UserDuplicatedEmailException;
+import com.ahincho.healthcare.domain.exceptions.UserDuplicatedUsernameException;
+import com.ahincho.healthcare.domain.mappers.UserMapper;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UserService userService;
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping
+    public ResponseEntity<UserResponse> register(@RequestBody @Valid UserRequest userRequest) throws UserDuplicatedEmailException, UserDuplicatedUsernameException, RoleNotFoundException {
+        UserEntity savedUserEntity = userService.createUser(UserMapper.requestToEntity(userRequest));
+        return ResponseEntity.ok(UserMapper.entityToResponse(savedUserEntity));
+    }
+    /*
+    @PostMapping("/login")
+    public ResponseEntity<UserResponse> login(@RequestBody @Valid LoginRequest loginRequest) throws UserNotFoundException {
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+        UserEntity userEntity = userService.findUserByEmailAndPassword(email, password);
+        return ResponseEntity.ok(UserMapper.entityToResponse(userEntity));
+    }
+    */
+}
