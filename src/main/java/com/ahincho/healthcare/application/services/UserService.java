14a36b4587d61669ab4f1110f9f0038d65b8f6e8
ahincho <ahincho@unsa.edu.pe>
2023-12-25 18:31:17 -0500

    Add User Role as Default and Recover Roles

--- src/main/java/com/ahincho/healthcare/application/services/UserService.java
+++ src/main/java/com/ahincho/healthcare/application/services/UserService.java
@@ -1,20 +1,26 @@
 package com.ahincho.healthcare.application.services;
 
+import com.ahincho.healthcare.domain.entities.RoleEntity;
 import com.ahincho.healthcare.domain.entities.UserEntity;
+import com.ahincho.healthcare.domain.exceptions.RoleNotFoundException;
 import com.ahincho.healthcare.domain.exceptions.UserDuplicatedEmailException;
 import com.ahincho.healthcare.domain.exceptions.UserDuplicatedUsernameException;
 import com.ahincho.healthcare.domain.exceptions.UserNotFoundException;
+import com.ahincho.healthcare.domain.repositories.RoleRepository;
 import com.ahincho.healthcare.domain.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    public UserService(UserRepository userRepository) {
+    private final RoleRepository roleRepository;
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
     public List<UserEntity> getAllUsers() {
         return userRepository.findAll();
@@ -25,11 +31,14 @@ public class UserService {
         return optionalUser.get();
     }
     @Transactional
-    public UserEntity createUser(UserEntity userEntity) throws UserDuplicatedEmailException, UserDuplicatedUsernameException {
+    public UserEntity createUser(UserEntity userEntity) throws UserDuplicatedEmailException, UserDuplicatedUsernameException, RoleNotFoundException {
         Optional<UserEntity> optionalUserEmail = userRepository.findByEmail(userEntity.getEmail());
         if (optionalUserEmail.isPresent()) { throw new UserDuplicatedEmailException(); }
         Optional<UserEntity> optionalUserUsername = userRepository.findByUsername(userEntity.getUsername());
         if (optionalUserUsername.isPresent()) { throw new UserDuplicatedUsernameException(); }
+        Optional<RoleEntity> roleEntity = roleRepository.findByName("USER");
+        if (roleEntity.isEmpty()) { throw new RoleNotFoundException(); }
+        userEntity.setRoles(Set.of(roleEntity.get()));
         return userRepository.save(userEntity);
     }
     @Transactional
@@ -37,6 +46,7 @@ public class UserService {
         Optional<UserEntity> optionalUser = userRepository.findById(id);
         if (optionalUser.isEmpty()) { throw new UserNotFoundException(); }
         userEntity.setId(id);
+        userEntity.setRoles(optionalUser.get().getRoles());
         userRepository.save(userEntity);
     }
     @Transactional
