bf40a9489fd83baa861a37b1d07fe9cbcdddea1a
ahincho <ahincho@unsa.edu.pe>
2023-12-24 22:36:47 -0500

    Adding Email and Username Duplicated Exceptions

--- src/main/java/com/ahincho/healthcare/domain/exceptions/UserDuplicatedException.java
+++
@@ -1,9 +0,0 @@
-package com.ahincho.healthcare.domain.exceptions;
-
-import static com.ahincho.healthcare.domain.exceptions.UserExceptionMessages.DUPLICATED;
-
-public class UserDuplicatedException extends Exception {
-    public UserDuplicatedException() {
-        super(DUPLICATED);
-    }
-}
