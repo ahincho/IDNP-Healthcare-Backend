813d6fdcdde4529ba4e65ecc2cacf60ccce527c4
ahincho <ahincho@unsa.edu.pe>
2023-12-25 21:31:43 -0500

    Username Not Found Handler

--- src/main/java/com/ahincho/healthcare/infrastructure/handlers/UserExceptionHandler.java
+++ src/main/java/com/ahincho/healthcare/infrastructure/handlers/UserExceptionHandler.java
@@ -5,6 +5,7 @@ import com.ahincho.healthcare.domain.exceptions.UserDuplicatedUsernameException;
 import com.ahincho.healthcare.domain.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,5 +22,9 @@ public class UserExceptionHandler {
     @ExceptionHandler(UserDuplicatedUsernameException.class)
     public ResponseEntity<Void> userDuplicatedUsername() {
         return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<Void> usernameNotFound() {
+        return ResponseEntity.notFound().build();
     }
 }
