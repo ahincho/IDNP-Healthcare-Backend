9c1ece8eb810e54aeef37529c6e3edbeb8071201
ahincho <ahincho@unsa.edu.pe>
2023-12-25 23:08:20 -0500

    Enabling Method Security by Roles

--- src/main/java/com/ahincho/healthcare/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/ahincho/healthcare/infrastructure/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
     private final UserDetailsServiceImpl userDetailsService;
     private final JwtUtils jwtUtils;
