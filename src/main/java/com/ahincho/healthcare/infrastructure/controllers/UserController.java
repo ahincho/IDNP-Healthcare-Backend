940585ef40a0c4f993c88e48f035e46d8cf60f0b
ahincho <ahincho@unsa.edu.pe>
2023-12-25 23:30:49 -0500

    Authority Access for User Endpoint

--- src/main/java/com/ahincho/healthcare/infrastructure/controllers/UserController.java
+++ src/main/java/com/ahincho/healthcare/infrastructure/controllers/UserController.java
@@ -11,6 +11,7 @@ import com.ahincho.healthcare.domain.exceptions.UserDuplicatedUsernameException;
 import com.ahincho.healthcare.domain.exceptions.UserNotFoundException;
 import com.ahincho.healthcare.domain.mappers.UserMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import jakarta.validation.Valid;
@@ -25,28 +26,33 @@ public class UserController {
         this.userService = userService;
     }
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<UserResponse>> getAll() {
         List<UserEntity> userEntities = userService.getAllUsers();
         if (userEntities.isEmpty()) { return ResponseEntity.noContent().build(); }
         return ResponseEntity.ok(userEntities.stream().map(UserMapper::entityToResponse).toList());
     }
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<UserResponse> findById(@PathVariable("id") Integer id) throws UserNotFoundException {
         UserEntity userEntity = userService.findUserById(id);
         return ResponseEntity.ok(UserMapper.entityToResponse(userEntity));
     }
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<UserResponse> save(@RequestBody @Valid UserRequest userRequest, UriComponentsBuilder uriComponentsBuilder) throws UserDuplicatedEmailException, UserDuplicatedUsernameException, RoleNotFoundException {
         UserEntity savedUserEntity = userService.createUser(UserMapper.requestToEntity(userRequest));
         URI uri = uriComponentsBuilder.path("/api/users/{id}").buildAndExpand(savedUserEntity.getId()).toUri();
         return ResponseEntity.created(uri).body(UserMapper.entityToResponse(savedUserEntity));
     }
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> update(@PathVariable("id") Integer id, @RequestBody @Valid UserUpdateRequest userUpdateRequest) throws UserNotFoundException, UserDuplicatedUsernameException {
         userService.updateUser(id, UserMapper.updateRequestToEntity(userUpdateRequest));
         return ResponseEntity.notFound().build();
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id) throws UserNotFoundException {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
