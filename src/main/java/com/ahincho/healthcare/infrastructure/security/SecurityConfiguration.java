9ddcecdee674c26d7ec1b27c21651ade008113ca
ahincho <ahincho@unsa.edu.pe>
2023-12-25 20:17:50 -0500

    Expose Register Rest Method

--- src/main/java/com/ahincho/healthcare/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/ahincho/healthcare/infrastructure/security/SecurityConfiguration.java
@@ -2,12 +2,16 @@ package com.ahincho.healthcare.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,7 +22,7 @@ public class SecurityConfiguration {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/auth").permitAll();
+                    auth.requestMatchers("/api/auth/register").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .sessionManagement(session ->
@@ -32,5 +36,15 @@ public class SecurityConfiguration {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("Angel").password("ahincho").roles().build());
         return manager;
+    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
     }
 }
