6e7293dacbe6e5855b0bea42c98db50380e8a65f
ahincho <ahincho@unsa.edu.pe>
2023-12-28 09:59:09 -0500

    Adding Repository Annotation for Context Loader

--- src/main/java/com/ahincho/healthcare/domain/repositories/RoleRepository.java
+++ src/main/java/com/ahincho/healthcare/domain/repositories/RoleRepository.java
@@ -2,8 +2,10 @@ package com.ahincho.healthcare.domain.repositories;
 
 import com.ahincho.healthcare.domain.entities.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
     Optional<RoleEntity> findByName(String name);
 }
